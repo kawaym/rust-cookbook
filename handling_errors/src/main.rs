6c9ee0958d5d88901638ff126532e77c3e1732e4
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-07-24 14:19:34 -0300

    adds handling_errors. Chapter 9

--- handling_errors/src/main.rs
+++ handling_errors/src/main.rs
@@ -1,7 +1,8 @@
+use std::error::Error;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // panic!("crash and burn!");
 
     // let v = vec![1, 2, 3];
@@ -26,4 +27,26 @@ fn main() {
 
     let greeting_file =
         File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+
+    // let mut username_file = match username_file_result {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // }
+
+    // let mut username = String::new();
+
+    // match username_file.read_to_string(&mut username) {
+    //     Ok(_) => Ok(username),
+    //     Err(e) => Err(e),
+    // }
+
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
