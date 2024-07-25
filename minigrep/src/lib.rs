034879b7b223391d29e031ebe916fc0848077fd0
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-07-24 23:37:36 -0300

    refactors minigrep. Chapter 12.4

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,14 +1,6 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -24,5 +16,44 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
